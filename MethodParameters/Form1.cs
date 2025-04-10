29e268a6d30348fc87ae2501ceb8e92ee74a0581
Tony Theodoropoulos <theodot@outlook.com>
2025-04-10 14:11:17 -0400

    completed demo

--- MethodParameters/Form1.cs
+++ MethodParameters/Form1.cs
@@ -23,15 +23,19 @@ namespace MethodParameters
             int num1 = Convert.ToInt16(q1Num1Input.Text);
             int num2 = Convert.ToInt16(q1Num2Input.Text);
             Add(num1, num2);
-
+            
             //get value for question 2 and send it to the CanVote method
-            int age = Convert.ToInt16(ageInput.Text);          
+            int age = Convert.ToInt16(ageInput.Text);
+            CanVote(age); 
 
             //get values for question 3 and send them to the WelcomeUser method
-
+            string username = userInput.Text;   
+            string pin = pinInput.Text;
+            WelcomeUser(username, pin);
 
             //get value for question 4 and send it to the InchToCm method
-
+            double valueToConvert = Convert.ToDouble(inchesInput.Text);
+            InchToCm(valueToConvert);
 
         }
 
@@ -49,7 +53,8 @@ namespace MethodParameters
 
         public void Add(int x, int y)
         {
-
+            int sum = x + y;
+            q1Output.Text = $"{x} + {y} = {sum}";
         }
 
         /// 2. Create a method called CanVote, that accepts an 
@@ -63,7 +68,17 @@ namespace MethodParameters
         /// 
         /// Input: 15
         /// You are not eligible to vote
-
+        public void CanVote(int voterAge)
+        {
+            if (voterAge >= 18)
+            {
+                q2Output.Text = "You are eligible to vote";
+            }
+            else
+            {
+                q2Output.Text = "You are not eligible to vote";
+            }
+        }
 
 
         /// 3. Create a method called WelcomeUser, that accepts 2
@@ -78,7 +93,17 @@ namespace MethodParameters
         /// 
         /// Input: Mr. T, 1234
         /// Welcome Mr. T
-
+        public void WelcomeUser(string name, string pin)
+        {
+            if(pin == "1234")
+            {
+                q3Output.Text = $"Welcome {name}";
+            }
+            else
+            {
+                q3Output.Text = "Incorrect PIN";
+            }
+        }
 
 
         /// 4. Create a method called InchToCm, that accepts a double 
@@ -88,11 +113,15 @@ namespace MethodParameters
         /// Sample input and output:
         /// 
         /// Input: 2
-        /// 2 inches is 5.08 cms
+        /// 2 inches is 5.08 cm
         /// 
         /// Input: 3.4
-        /// 3.4 inches is 8.636 cms
-
+        /// 3.4 inches is 8.636 cm
+        public void InchToCm(double inches)
+        {
+            double cm = inches * 2.54;
+            q4Output.Text = $"{inches} inches is {cm} cm";
+        }
 
 
     }
